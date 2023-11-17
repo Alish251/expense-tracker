a4ea9a89e526c771966eb14f6388dff33eda8955
anogin <ANogin@kazeuromobile.kz>
2023-11-17 16:58:36 +0600

    fixed pom.xml

--- src/main/java/org/expensetracker/service/impl/AccountServiceImpl.java
+++ src/main/java/org/expensetracker/service/impl/AccountServiceImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service(value = "test")
+@Service
 public class AccountServiceImpl implements AccountService {
     private final AccountRepository repository;
     private final AccountMapper mapper;
